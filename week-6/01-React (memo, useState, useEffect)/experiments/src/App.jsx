d3df5cad702b61cae31404450c41dbee553fa5bf
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-03-14 16:00:14 +0530

    React

---
+++ week-6/01-React (memo, useState, useEffect)/experiments/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+function Todos(props) {
+  return (
+    <div>
+      <h1> {props.title} </h1>
+      <h2> {props.description} </h2>
+    </div>
+  );
+}
+
+function RenderTodos({ todos }) {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <Todos
+          key={todo.id}
+          title={todo.title}
+          description={todo.description}
+        ></Todos>
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  async function fetchTodos() {
+    const todos = await fetch("http://localhost:3000/todos");
+    const json = await todos.json();
+    setTodos(json.todos);
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      fetchTodos();
+    }, 2000);
+  }, []);
+
+  return (
+    <div>
+      <RenderTodos todos={todos}></RenderTodos>
+    </div>
+  );
+}
+
+export default App;
