5aa385ad38cda1bf8b97a8fc0cc7d2c24063390a
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-03-29 18:06:55 +0530

    context api basics

---
+++ week-7/02-React/1-Button-Context/1-Button.jsx
@@ -0,0 +1,49 @@
+import { useContext, useState } from "react";
+import { CountContext } from "./components/context";
+
+function App(){
+
+  const [count , setCount] = useState(0);
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+      <Count setCount = {setCount}/>
+      </CountContext.Provider>
+    </div>
+  )
+}
+
+function Count({setCount}){
+  console.log("count re-render")
+  return (
+    <div>
+      <CountRenderer/>
+      <Buttons setCount={setCount}/>
+    </div>
+  )
+}
+
+function CountRenderer(){
+  let count = useContext(CountContext);
+  return(
+    <div>
+      {count}
+    </div>
+  )
+}
+
+function Buttons({setCount}){
+  let count = useContext(CountContext);
+  return(
+    <div>
+      <button onClick={()=>{
+        setCount(count+1);
+      }}>Increase</button>
+      <button onClick={()=>{
+        setCount( count-1);
+      }}>Decrease</button>
+    </div>
+  )
+}
+
+export default App;
