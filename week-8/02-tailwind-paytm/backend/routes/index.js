74fa7c4a2b4e257ade388c372a49ac0f571627f9
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-04-01 14:35:37 +0530

    Routes

---
+++ week-8/02-tailwind-paytm/backend/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const userRouter = require("./user")
+
+const router = express.Router();
+router.use("/user", userRouter)
+
+router.get("/", (req,res) => {
+
+})
+
+module.exports = router
