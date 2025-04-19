2c03df928388c72d4200eff2e3faea5f23060eaa
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-04-19 19:09:20 +0530

    added accounts db

--- week-8/02-tailwind-paytm/backend/db.js
+++ week-8/02-tailwind-paytm/backend/db.js
@@ -40,9 +40,25 @@ const userSchem = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model('Users', userSchem);
 
+const accountSchema = new mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    },
+    balance : {
+        type : Number,
+        required : true
+    }
+})
+
+
+
+const User = mongoose.model('Users', userSchem);
+const Account = mongoose.model('Account', accountSchema);
 
 module.exports = {
-    User
+    User,
+    Account
 }
