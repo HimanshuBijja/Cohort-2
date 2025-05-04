302d7e4302928dac824b83157b34f7e77ebf6119
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-05-05 00:08:12 +0530

    commit

--- week-10/01-Postgres/src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from "pg";
-
-export async function getClient() {
-	const client = new Client("postgresql://postgres:123@localhost:5432/demo");
-	await client.connect();
-	return client;
-}
