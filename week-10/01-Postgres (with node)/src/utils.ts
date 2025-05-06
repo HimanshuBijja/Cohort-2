40b0594ee85d7887e347c486de58b813b5adf582
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-05-06 23:32:38 +0530

    secure pg config

--- week-10/01-Postgres (with node)/src/utils.ts
+++ week-10/01-Postgres (with node)/src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from "pg";
 
 export async function getClient() {
-	const client = new Client("postgresql://postgres:123@localhost:5432/demo");
+	const client = new Client("Connection string");
 	await client.connect();
 	return client;
 }
