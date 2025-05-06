dc955678eed6b1c345877c2d8b59c30f533f0838
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-05-06 23:27:45 +0530

    commit

--- week-10/03-postgress/DB-Queries.session.sql
+++ week-10/03-postgress/DB-Queries.session.sql
@@ -1,3 +1,7 @@
+-- CREATE`
+-- `
+--
+--
 CREATE TABLE users (
     id serial PRIMARY KEY,
     username VARCHAR(30) UNIQUE NOT NULL,
@@ -5,7 +9,6 @@ CREATE TABLE users (
     user_password VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
 CREATE TABLE user_address (
     id serial PRIMARY KEY,
     user_id INTEGER NOT NULL,
@@ -16,3 +19,53 @@ CREATE TABLE user_address (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+--
+--
+--
+--
+-- INSERT
+
+INSERT INTO users (username, email, user_password)
+VALUES ('himanshu', 'himanshu@123', 'pass')
+RETURNING id;
+INSERT INTO user_address (user_id, street, city, state, pincode)
+VALUES (2, 'Street-3', 'Hyd', 'Ts', '500076');
+--
+--
+--
+--
+--
+-- SELECT
+SELECT *
+FROM users;
+SELECT *
+FROM user_address;
+--joins
+--
+--
+--
+--
+SELECT username,
+    email,
+    street,
+    city,
+    state,
+    pincode
+FROM users
+    LEFT JOIN user_address ON users.id = user_address.user_id
+where users.id = 4;
+-- TYPE two
+--
+SELECT u.username,
+    u.email,
+    a.street,
+    a.city,
+    a.state,
+    a.pincode
+FROM users u
+    LEFT JOIN user_address a ON u.id = a.user_id
+where u.id = 4;
+--
+SELECT *
+FROM users
+    CROSS JOIN user_address;
