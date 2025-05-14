1ba69171438552b128dcb52852f0f562d972f714
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-05-14 22:01:15 +0530

    commit

---
+++ week-12-Nextjs/04-next-auth/src/app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Login with email",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        const username = credentials?.username
+        const password = credentials?.password
+        console.log(username);  
+        console.log(password);
+        const user = {
+            name : "himanshu",
+            id : "1",
+            username : "himanshu@123"
+        }
+
+        if(user){
+            return user;
+        }
+        else{
+            return null;
+        }
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
